6bdbe01892e87e4a14a1110784cd5927f13fa107
senugbin9850 <tanja1206@naver.com>
2021-03-29 11:48:44 +0900

    [ADD] connection

--- src/main/server.ts
+++ src/main/server.ts
@@ -2,9 +2,14 @@ import "module-alias/register";
 import "reflect-metadata";
 import env from "@/main/config/env";
 import app from "@/main/config/app";
+import { connection } from "@/infra/db";
 
 const PORT: number = env.port;
 
-app.listen(PORT, () => {
-  console.log(`server on port ${PORT}`);
-});
+connection()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`server on port ${PORT}`);
+    });
+  })
+  .catch(console.error);
