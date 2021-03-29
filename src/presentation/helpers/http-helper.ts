4abc34f40f3b18d45db2c353aa37ff831fb427a2
senugbin9850 <tanja1206@naver.com>
2021-03-29 11:14:51 +0900

    [ADD] http helper

---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,36 @@
+import { HttpResponse } from "@/presentation/protocols";
+import {
+  AlreadyExistsError,
+  ServerError,
+  UnauthorizedError,
+} from "@/presentation/errors";
+
+export const badRequest = (error: Error): HttpResponse => ({
+  statusCode: 400,
+  body: error,
+});
+
+export const unauthorized = (): HttpResponse => ({
+  statusCode: 401,
+  body: new UnauthorizedError(),
+});
+
+export const forbidden = (error: Error): HttpResponse => ({
+  statusCode: 403,
+  body: error,
+});
+
+export const alreadyExistsData = (): HttpResponse => ({
+  statusCode: 409,
+  body: new AlreadyExistsError(),
+});
+
+export const serverError = (error: Error): HttpResponse => ({
+  statusCode: 500,
+  body: new ServerError(error.stack),
+});
+
+export const ok = (data: any): HttpResponse => ({
+  statusCode: 200,
+  body: data,
+});
