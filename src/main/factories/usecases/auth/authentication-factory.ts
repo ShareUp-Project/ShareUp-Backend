23ffe0d0af55f729312e8dd7e75c237b986267c8
senugbin9850 <tanja1206@naver.com>
2021-03-30 22:47:47 +0900

    [ADD] authentication factory usecase

---
+++ src/main/factories/usecases/auth/authentication-factory.ts
@@ -0,0 +1,16 @@
+import { DbAuthentication } from "@/data/usecases";
+import { Authentication } from "@/domain/usecase";
+import { BcryptAdapter } from "@/infra/crypto";
+import { UserRepository } from "@/infra/db";
+import { JwtAdapter } from "@/infra/jwt";
+import env from "@/main/config/env";
+
+export const makeDbAuthentication = (): Authentication => {
+  const SALT = 12;
+  const ACCESS_SECRET = env.jwt.access;
+  const REFRESH_SECRET = env.jwt.refresh;
+  const findUserRepository = new UserRepository();
+  const bcryptAdapter = new BcryptAdapter(SALT);
+  const jwtAdapter = new JwtAdapter(ACCESS_SECRET, REFRESH_SECRET);
+  return new DbAuthentication(findUserRepository, bcryptAdapter, jwtAdapter);
+};
