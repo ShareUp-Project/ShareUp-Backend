d141553357585e89694c71f3ed2ff217717c2910
senugbin9850 <tanja1206@naver.com>
2021-03-30 16:54:07 +0900

    [ADD] routing user api

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,6 +1,10 @@
 import { Express, Router } from "express";
 
+import userRouter from "@/main/routes/user";
+
 export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
+
+  userRouter(router);
 };
