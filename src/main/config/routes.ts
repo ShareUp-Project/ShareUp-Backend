7313118f860eaf7d28afb9d606645e5addb4dbdc
senugbin9850 <tanja1206@naver.com>
2021-03-30 22:41:29 +0900

    [ADD] auth router

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,12 @@
 import { Express, Router } from "express";
 
 import userRouter from "@/main/routes/user";
+import authRouter from "@/main/routes/auth";
 
 export default (app: Express): void => {
   const router = Router();
   app.use("/api", router);
 
   userRouter(router);
+  authRouter(router);
 };
