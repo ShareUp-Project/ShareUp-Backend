43b433feff553728ed3a92075171f6761f9ff367
senugbin9850 <tanja1206@naver.com>
2021-03-30 19:13:38 +0900

    [ADD] create user schema

---
+++ src/validator/schemas/user/create-user.ts
@@ -0,0 +1,9 @@
+import Joi from "joi";
+
+export const CreateUserSchema = Joi.object().keys({
+  phone: Joi.string().length(11).required(),
+  password: Joi.string()
+    .regex(/^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .required(),
+  nickname: Joi.string().min(2).max(10).required(),
+});
