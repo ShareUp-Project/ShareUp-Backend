f3904151cc2f11783ef4cb9422a057c4e60aeb5b
senugbin9850 <tanja1206@naver.com>
2021-03-30 16:53:57 +0900

    [FIX] express route blocking

--- src/main/adapters/express-route.ts
+++ src/main/adapters/express-route.ts
@@ -1,7 +1,7 @@
 import { Controller } from "@/presentation/protocols";
 import { Request, Response } from "express";
 
-export const adaptRoute = async (controller: Controller) => {
+export const adaptRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const request = {
       ...(req.body || {}),
