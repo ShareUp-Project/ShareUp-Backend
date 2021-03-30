d66bc1abda0e8b980530d036805eef393a326c3d
senugbin9850 <tanja1206@naver.com>
2021-03-30 19:12:43 +0900

    [ADD] use validation middleware

--- src/main/routes/user.ts
+++ src/main/routes/user.ts
@@ -1,8 +1,17 @@
 import { adaptRoute } from "@/main/adapters";
 import { makeSignupController } from "@/main/factories";
+import { Parameters, validationMiddleware } from "@/main/middlewares";
+import { CreateUserSchema } from "@/validator/schemas";
 
 import { Router } from "express";
 
 export default (router: Router): void => {
-  router.post("/user", adaptRoute(makeSignupController()));
+  router.post(
+    "/user",
+    validationMiddleware({
+      schema: CreateUserSchema,
+      parameters: Parameters.BODY,
+    }),
+    adaptRoute(makeSignupController())
+  );
 };
