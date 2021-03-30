1836b7d83ae7a2f420729019e26bcc041e911475
senugbin9850 <tanja1206@naver.com>
2021-03-30 16:50:59 +0900

    [ADD] make uuid

--- src/data/usecases/db-create-user.ts
+++ src/data/usecases/db-create-user.ts
@@ -1,17 +1,20 @@
 import { CreateUser } from "@/domain/usecase";
-import { Hasher } from "../protocols";
-import { CreateUserRepository } from "../protocols/db/user";
+import { Hasher, MakeId } from "@/data/protocols";
+import { CreateUserRepository } from "@/data/protocols/db";
 
 export class DbCreateUser implements CreateUser {
   constructor(
+    private readonly createUserRepository: CreateUserRepository,
     private readonly hasher: Hasher,
-    private readonly createUserRepository: CreateUserRepository
+    private readonly makeId: MakeId
   ) {}
 
   async create(data: CreateUser.Params): Promise<void> {
+    const uuid = await this.makeId.makeId();
     const hashedPassword = await this.hasher.hash(data.password);
     await this.createUserRepository.create({
       ...data,
+      id: uuid,
       password: hashedPassword,
     });
   }
