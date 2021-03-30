9939c2e6678c42e1a0b492b667d09edfee13a8ac
senugbin9850 <tanja1206@naver.com>
2021-03-30 22:41:12 +0900

    [ADD] jwt adapter

---
+++ src/infra/jwt/jwt-adapter.ts
@@ -0,0 +1,32 @@
+import { Decrypter, Encrypter, TokenType } from "@/data/protocols";
+
+import { sign, verify } from "jsonwebtoken";
+
+export class JwtAdapter implements Encrypter, Decrypter {
+  constructor(
+    private readonly accessSecret: string,
+    private readonly refreshSecret: string
+  ) {}
+
+  async encrypt(plaintext: string, type: TokenType): Promise<string> {
+    switch (type) {
+      case "access":
+        return sign({ phone: plaintext }, this.accessSecret, {
+          expiresIn: "1h",
+        });
+      case "refresh":
+        return sign({ phone: plaintext }, this.refreshSecret, {
+          expiresIn: "1w",
+        });
+    }
+  }
+
+  async decrypt(ciphertext: string, type: TokenType): Promise<string> {
+    switch (type) {
+      case "access":
+        return verify(ciphertext, this.accessSecret) as any;
+      case "refresh":
+        return verify(ciphertext, this.refreshSecret) as any;
+    }
+  }
+}
