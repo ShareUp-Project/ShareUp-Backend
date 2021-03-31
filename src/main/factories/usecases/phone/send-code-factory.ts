d6554dd20d1fbdb6c8f35ac2d8c43547cede6547
senugbin9850 <tanja1206@naver.com>
2021-03-31 14:09:07 +0900

    [ADD] send code factory

---
+++ src/main/factories/usecases/phone/send-code-factory.ts
@@ -0,0 +1,22 @@
+import { DbSendCode } from "@/data/usecases/phone/db-send-code";
+import { SendCode } from "@/domain/usecase";
+import { CodeAdapter } from "@/infra/code";
+import { RedisRepository } from "@/infra/db/redis/redis-repository";
+import { SendMessageAdapter } from "@/infra/phone";
+import env from "@/main/config/env";
+
+export const makeDbSendCode = (): SendCode => {
+  const SERVICE_ID = env.sens.id;
+  const ACCESS = env.sens.access;
+  const SECRET = env.sens.secret;
+  const PHONE = env.sens.phone;
+  const redisRepository = new RedisRepository();
+  const codeAdapter = new CodeAdapter();
+  const sendMessageAdapter = new SendMessageAdapter(
+    SERVICE_ID,
+    ACCESS,
+    SECRET,
+    PHONE
+  );
+  return new DbSendCode(redisRepository, codeAdapter, sendMessageAdapter);
+};
