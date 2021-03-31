3304280a69a801e211deb1908bad80dc4b98ed00
senugbin9850 <tanja1206@naver.com>
2021-03-31 14:12:01 +0900

    [ADD] check already exists user

--- src/presentation/controllers/phone/send-code-controller.ts
+++ src/presentation/controllers/phone/send-code-controller.ts
@@ -1,12 +1,17 @@
-import { SendCode } from "@/domain/usecase";
-import { ok, serverError } from "@/presentation/helpers";
+import { FindUser, SendCode } from "@/domain/usecases";
+import { alreadyExistsData, ok, serverError } from "@/presentation/helpers";
 import { Controller, HttpResponse } from "@/presentation/protocols";
 
 export class SendCodeController implements Controller {
-  constructor(private readonly sendCode: SendCode) {}
+  constructor(
+    private readonly sendCode: SendCode,
+    private readonly findUser: FindUser
+  ) {}
 
   async handle(request: SendCodeController.Request): Promise<HttpResponse> {
     try {
+      const user = await this.findUser.findOne(request.phone);
+      if (user) return alreadyExistsData();
       this.sendCode.send(request.phone);
       return ok({ message: "success" });
     } catch (e) {
