cca0412dda9eab599969375207c7f05bed8dd8a8
senugbin9850 <tanja1206@naver.com>
2021-03-31 14:52:14 +0900

    [ADD] phone api validation check

--- src/main/routes/phone.ts
+++ src/main/routes/phone.ts
@@ -1,9 +1,17 @@
 import { adaptRoute } from "@/main/adapters";
 import { makeSendCodeController } from "@/main/factories";
 import { Parameters, validationMiddleware } from "@/main/middlewares";
+import { SendCodeSchema } from "@/validator/schemas";
 
 import { Router } from "express";
 
 export default (router: Router): void => {
-  router.post("/phone", adaptRoute(makeSendCodeController()));
+  router.post(
+    "/phone",
+    validationMiddleware({
+      schema: SendCodeSchema,
+      parameters: Parameters.BODY,
+    }),
+    adaptRoute(makeSendCodeController())
+  );
 };
