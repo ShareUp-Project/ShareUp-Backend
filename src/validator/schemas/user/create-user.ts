944b58c559b2bfc9ba3a5f6ce3d516bdd804f1a3
senugbin9850 <tanja1206@naver.com>
2021-03-31 15:12:03 +0900

    [FIX] authentication schema password must include big letter

--- src/validator/schemas/user/create-user.ts
+++ src/validator/schemas/user/create-user.ts
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const CreateUserSchema = Joi.object().keys({
   phone: Joi.string().length(11).required(),
   password: Joi.string()
-    .regex(/^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
+    .regex(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/)
     .required(),
   nickname: Joi.string().min(2).max(10).required(),
 });
