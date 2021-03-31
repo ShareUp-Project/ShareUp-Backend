d42817ceef1bb6bd70a530e7c7412a99b1245241
senugbin9850 <tanja1206@naver.com>
2021-03-31 14:04:54 +0900

    [ADD] redis repository

---
+++ src/infra/db/redis/redis-repository.ts
@@ -0,0 +1,18 @@
+import { GetAuthRepository, SetAuthRepository } from "@/data/protocols/db";
+import redis from "redis";
+
+const client = redis.createClient();
+
+export class RedisRepository implements SetAuthRepository, GetAuthRepository {
+  setAuthCode(data: SetAuthRepository.Params): void {
+    client.set(data.phone, data.code);
+    client.expire(data.phone, 180);
+  }
+
+  getAuthCode(phone: string): string {
+    client.get(phone, (e, data) => {
+      return data;
+    });
+    return "";
+  }
+}
