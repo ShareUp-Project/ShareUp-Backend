03522c92333ca96c6147d5e6acf55bd13ccea082
senugbin9850 <tanja1206@naver.com>
2021-03-31 14:17:53 +0900

    [FIX] send code controller parameters

--- src/main/factories/controllers/phone/send-code-controller-factory.ts
+++ src/main/factories/controllers/phone/send-code-controller-factory.ts
@@ -1,7 +1,8 @@
 import { SendCodeController } from "@/presentation/controllers";
 import { Controller } from "@/presentation/protocols";
 import { makeDbSendCode } from "@/main/factories";
+import { makeDbFindUser } from "../../usecases";
 
 export const makeSendCodeController = (): Controller => {
-  return new SendCodeController(makeDbSendCode());
+  return new SendCodeController(makeDbSendCode(), makeDbFindUser());
 };
