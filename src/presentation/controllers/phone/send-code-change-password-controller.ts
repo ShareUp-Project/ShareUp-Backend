26726a579856fbc4b951ee3b4389a85832f0c4e8
senugbin9850 <tanja1206@naver.com>
2021-04-01 20:25:18 +0900

    [ADD] send code change password controller

---
+++ src/presentation/controllers/phone/send-code-change-password-controller.ts
@@ -0,0 +1,29 @@
+import { FindUser, SendCode } from "@/domain/usecases";
+import { notFound, ok, serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+
+export class SendCodeChangePasswordController implements Controller {
+  constructor(
+    private readonly sendCode: SendCode,
+    private readonly findUser: FindUser
+  ) {}
+
+  async handle(
+    request: SendCodeChangePasswordController.Request
+  ): Promise<HttpResponse> {
+    try {
+      const user = await this.findUser.findOne(request.phone);
+      if (!user) return notFound();
+      this.sendCode.send(request.phone);
+      return ok({ message: "success" });
+    } catch (e) {
+      return serverError(e);
+    }
+  }
+}
+
+export namespace SendCodeChangePasswordController {
+  export type Request = {
+    phone: string;
+  };
+}
