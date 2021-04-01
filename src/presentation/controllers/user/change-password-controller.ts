fe3bb0768924ea9b3c8b34dc10d62ea33f94d8af
senugbin9850 <tanja1206@naver.com>
2021-04-01 11:43:39 +0900

    [ADD] change password controller

---
+++ src/presentation/controllers/user/change-password-controller.ts
@@ -0,0 +1,30 @@
+import { ChangePassword, FindUser } from "@/domain/usecases";
+import { notFound, ok, serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+
+export class ChangePasswordController implements Controller {
+  constructor(
+    private readonly findUser: FindUser,
+    private readonly changePassword: ChangePassword
+  ) {}
+
+  async handle(
+    request: ChangePasswordController.Request
+  ): Promise<HttpResponse> {
+    try {
+      const user = await this.findUser.findOne(request.phone);
+      if (!user) return notFound();
+      await this.changePassword.change(request);
+      return ok({ message: "success" });
+    } catch (e) {
+      return serverError(e);
+    }
+  }
+}
+
+export namespace ChangePasswordController {
+  export type Request = {
+    phone: string;
+    password: string;
+  };
+}
