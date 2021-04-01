259f587ecc54f41f9a533b2f2f15255c105eee97
senugbin9850 <tanja1206@naver.com>
2021-04-01 11:42:48 +0900

    [ADD] change password usecase

---
+++ src/data/usecases/user/db-change-password.ts
@@ -0,0 +1,17 @@
+import { ChangePasswordRepository, Hasher } from "@/data/protocols";
+import { ChangePassword } from "@/domain/usecases";
+
+export class DbChangePassword implements ChangePassword {
+  constructor(
+    private readonly changePasswordRepository: ChangePasswordRepository,
+    private readonly hasher: Hasher
+  ) {}
+
+  async change(data: ChangePassword.Params): Promise<void> {
+    const hashedPassword = await this.hasher.hash(data.password);
+    await this.changePasswordRepository.change({
+      ...data,
+      password: hashedPassword,
+    });
+  }
+}
