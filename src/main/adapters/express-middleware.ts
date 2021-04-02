04fe39fbe8bee22109c4373091c680e713885143
senugbin9850 <tanja1206@naver.com>
2021-04-02 12:01:51 +0900

    [FIX] express middleware request header parameter name

--- src/main/adapters/express-middleware.ts
+++ src/main/adapters/express-middleware.ts
@@ -4,7 +4,7 @@ import { Request, Response, NextFunction } from "express";
 export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const request = {
-      accessToken: req.headers?.["authorization"],
+      token: req.headers?.["authorization"],
       ...(req.headers || {}),
     };
     const httpResponse = await middleware.handle(request);
