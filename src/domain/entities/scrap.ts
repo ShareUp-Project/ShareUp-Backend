bbe2f54fde93633a0cd5fbac300dff698f5f9bb3
senugbin9850 <tanja1206@naver.com>
2021-04-04 20:19:47 +0900

    [ADD] scrap enum

---
+++ src/domain/entities/scrap.ts
@@ -0,0 +1,25 @@
+import { Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
+import { User, Post } from "@/domain/entities";
+
+@Entity()
+export class Scrap {
+  @PrimaryColumn({ name: "user_id", length: 45 })
+  userId: string;
+
+  @PrimaryColumn({ name: "post_id", length: 45 })
+  postId: string;
+
+  @ManyToOne((type) => User, (user) => user.id, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "user_id" })
+  user!: User;
+
+  @ManyToOne((type) => Post, (post) => post.id, {
+    onUpdate: "CASCADE",
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "post_id" })
+  post!: Post;
+}
