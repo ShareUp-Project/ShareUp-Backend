f4678287c6cefa6b2511fcd9fed77e566b551251
senugbin9850 <tanja1206@naver.com>
2021-04-05 19:34:15 +0900

    [ADD] post router

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -3,6 +3,7 @@ import { Express, Router } from "express";
 import userRouter from "@/main/routes/user";
 import authRouter from "@/main/routes/auth";
 import phoneRouter from "@/main/routes/phone";
+import postRouter from "@/main/routes/post";
 
 export default (app: Express): void => {
   const router = Router();
@@ -11,4 +12,5 @@ export default (app: Express): void => {
   userRouter(router);
   authRouter(router);
   phoneRouter(router);
+  postRouter(router);
 };
