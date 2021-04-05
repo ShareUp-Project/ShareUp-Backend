42992956e266b5e3410c62f9f9868c5bf446a836
senugbin9850 <tanja1206@naver.com>
2021-04-05 11:13:39 +0900

    [ADD] write post usecase

---
+++ src/domain/usecases/post/write-post.ts
@@ -0,0 +1,14 @@
+import { Category } from "@/domain/entities";
+
+export interface WritePost {
+  write: (data: WritePost.Params) => Promise<string>;
+}
+
+export namespace WritePost {
+  export type Params = {
+    id: string;
+    content: string;
+    category: Category;
+    userId: string;
+  };
+}
