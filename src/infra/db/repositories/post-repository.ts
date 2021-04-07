02a265c4b4f685a7e7889ab10a44ffd1517cc3e0
senugbin9850 <tanja1206@naver.com>
2021-04-07 19:04:58 +0900

    [ADD] find post method

--- src/infra/db/repositories/post-repository.ts
+++ src/infra/db/repositories/post-repository.ts
@@ -1,9 +1,19 @@
-import { GetPostsRepository, WritePostRepository } from "@/data/protocols";
+import {
+  DeletePostRepository,
+  FindPostRepository,
+  GetPostsRepository,
+  WritePostRepository,
+} from "@/data/protocols";
 import { Post } from "@/domain/entities";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Post)
-export class PostRepository implements WritePostRepository, GetPostsRepository {
+export class PostRepository
+  implements
+    WritePostRepository,
+    GetPostsRepository,
+    DeletePostRepository,
+    FindPostRepository {
   public async write(data: WritePostRepository.Params): Promise<void> {
     await getRepository(Post)
       .createQueryBuilder("post")
@@ -23,5 +33,24 @@ export class PostRepository implements WritePostRepository, GetPostsRepository {
       .skip(data.page * 7)
       .take(7)
       .getMany();
+  }
+
+  public async delete(data: DeletePostRepository.Params): Promise<void> {
+    await getRepository(Post)
+      .createQueryBuilder("post")
+      .delete()
+      .from(Post)
+      .where("id = :id", { id: data.id })
+      .andWhere("userId = :userId", { userId: data.userId })
+      .execute();
+  }
+
+  public async findOne(
+    data: FindPostRepository.Params
+  ): Promise<FindPostRepository.Result> {
+    return await getRepository(Post)
+      .createQueryBuilder("post")
+      .where("id = :id", { id: data.id })
+      .getOne();
   }
 }
