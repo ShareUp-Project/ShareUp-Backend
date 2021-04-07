877c462123bc026caf0c44c9465d62e75e8df598
senugbin9850 <tanja1206@naver.com>
2021-04-07 14:46:24 +0900

    [ADD] get posts controller

--- src/presentation/controllers/post/get-posts-controller.ts
+++ src/presentation/controllers/post/get-posts-controller.ts
@@ -8,13 +8,19 @@ export class GetPostsController implements Controller {
   async handle(request: GetPostsController.Request): Promise<HttpResponse> {
     const data = await this.getPosts.get(request);
     const response = _.map(data, (e) => {
+      e.isScrap = false;
+      if (
+        e.scraps.findIndex(
+          (i) => i.userId === request.identity && i.postId === e.id
+        ) > -1
+      )
+        e.isScrap = true;
       e.scraps = e.scraps.length;
       e.hashtags = e.hashtags.map((e) => e.tag);
       e.images = e.images.map((e) => e.id);
       return _.pick(e, [
         "id",
         "title",
-        "content",
         "category",
         "views",
         "user.id",
@@ -22,6 +28,7 @@ export class GetPostsController implements Controller {
         "hashtags",
         "images",
         "scraps",
+        "isScrap",
       ]);
     });
     return ok({ data: response });
@@ -31,5 +38,6 @@ export class GetPostsController implements Controller {
 export namespace GetPostsController {
   export type Request = {
     page: number;
+    identity: string;
   };
 }
