2e304277b6852c6bc47092162ea7f17dd9a12996
senugbin9850 <tanja1206@naver.com>
2021-04-12 19:38:48 +0900

    [ADD] get scrap posts usecase

---
+++ src/data/usecases/post/db-get-scrap-posts.ts
@@ -0,0 +1,12 @@
+import { GetScrapPostsRepository } from "@/data/protocols";
+import { GetScrapPosts } from "@/domain/usecases";
+
+export class DbGetScrapPosts implements GetScrapPosts {
+  constructor(
+    private readonly getScrapPostsRepository: GetScrapPostsRepository
+  ) {}
+
+  async getScrap(data: GetScrapPosts.Params): Promise<any> {
+    return await this.getScrapPostsRepository.getScrap(data);
+  }
+}
