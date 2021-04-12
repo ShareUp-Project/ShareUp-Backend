80b6d0056d87c75dddc9a4be7cf0ca592c7de01f
senugbin9850 <tanja1206@naver.com>
2021-04-12 19:39:57 +0900

    [ADD] get scrap posts controller

---
+++ src/presentation/controllers/post/get-scrap-posts-controller.ts
@@ -0,0 +1,39 @@
+import { GetScrapPosts } from "@/domain/usecases";
+import { ok } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+import _ from "lodash";
+
+export class GetScrapPostsController implements Controller {
+  constructor(private readonly getScrapPosts: GetScrapPosts) {}
+
+  async handle(
+    request: GetScrapPostsController.Request
+  ): Promise<HttpResponse> {
+    const data = await this.getScrapPosts.getScrap({
+      userId: request.identity,
+      page: Number(request.page),
+    });
+    console.log(data);
+    const response = _.map(data, (e) => {
+      e.hashtags = e.hashtags.map((e) => e.tag);
+      e.images = e.images.map((e) => e.id);
+      return _.pick(e, [
+        "id",
+        "title",
+        "category",
+        "user.id",
+        "user.nickname",
+        "hashtags",
+        "images",
+      ]);
+    });
+    return ok({ data: response });
+  }
+}
+
+export namespace GetScrapPostsController {
+  export type Request = {
+    identity: string;
+    page: number;
+  };
+}
