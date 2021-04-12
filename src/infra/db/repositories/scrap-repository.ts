ae3fde0cf58ac8e69cc00568fe20f2beefb47426
senugbin9850 <tanja1206@naver.com>
2021-04-12 22:42:18 +0900

    [ADD] cancle scrap repository

--- src/infra/db/repositories/scrap-repository.ts
+++ src/infra/db/repositories/scrap-repository.ts
@@ -1,15 +1,29 @@
-import { ScrapPostRepository } from "@/data/protocols/db/scrap";
+import {
+  CancleScrapRepository,
+  ScrapPostRepository,
+} from "@/data/protocols/db/scrap";
 import { Scrap } from "@/domain/entities";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Scrap)
-export class ScrapRepository implements ScrapPostRepository {
+export class ScrapRepository
+  implements ScrapPostRepository, CancleScrapRepository {
   public async scrap(data: ScrapPostRepository.Params) {
     await getRepository(Scrap)
       .createQueryBuilder("scrap")
       .insert()
       .into(Scrap)
       .values(data)
+      .execute();
+  }
+
+  public async cancle(data: CancleScrapRepository.Params): Promise<void> {
+    await getRepository(Scrap)
+      .createQueryBuilder("scrap")
+      .delete()
+      .from(Scrap)
+      .where("userId = :userId", { userId: data.userId })
+      .andWhere("postId = :postId", { postId: data.id })
       .execute();
   }
 }
