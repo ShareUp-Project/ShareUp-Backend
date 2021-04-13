ccafa25b698b8fcd849fcf5b538dcdf587306a27
senugbin9850 <tanja1206@naver.com>
2021-04-13 10:33:58 +0900

    [ADD] get detail post usecase

---
+++ src/data/usecases/post/db-get-detail-post.ts
@@ -0,0 +1,12 @@
+import { GetDetailPostRepository } from "@/data/protocols";
+import { GetDetailPost } from "@/domain/usecases";
+
+export class DbGetDetailPost implements GetDetailPost {
+  constructor(
+    private readonly getDetailPostRepository: GetDetailPostRepository
+  ) {}
+
+  async getDetail(data: GetDetailPost.Params): Promise<any> {
+    return await this.getDetailPostRepository.getDetail(data);
+  }
+}
