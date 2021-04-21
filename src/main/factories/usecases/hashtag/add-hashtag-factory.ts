aae3f857ce7b342d6a57d955ec5642a9e3331fce
senugbin9850 <tanja1206@naver.com>
2021-04-21 19:44:47 +0900

    [DEL] uuid adapter

--- src/main/factories/usecases/hashtag/add-hashtag-factory.ts
+++ src/main/factories/usecases/hashtag/add-hashtag-factory.ts
@@ -1,10 +1,8 @@
 import { DbAddHashtag } from "@/data/usecases/hashtag";
 import { AddHashtag } from "@/domain/usecases";
 import { HashtagRepository } from "@/infra/db";
-import { UuidAdapter } from "@/infra/uuid";
 
 export const makeDbAddHashtag = (): AddHashtag => {
   const hashtagRepository = new HashtagRepository();
-  const uuidAdapter = new UuidAdapter();
-  return new DbAddHashtag(hashtagRepository, uuidAdapter);
+  return new DbAddHashtag(hashtagRepository);
 };
