9883b906ff5ed230c7b450dcfa08f389b6cded14
senugbin9850 <tanja1206@naver.com>
2021-04-21 19:44:07 +0900

    [DEL] make id

--- src/data/usecases/hashtag/db-add-hashtag.ts
+++ src/data/usecases/hashtag/db-add-hashtag.ts
@@ -2,13 +2,9 @@ import { AddHashtagRepository, MakeId } from "@/data/protocols";
 import { AddHashtag } from "@/domain/usecases";
 
 export class DbAddHashtag implements AddHashtag {
-  constructor(
-    private readonly addHashtagRepository: AddHashtagRepository,
-    private readonly makeId: MakeId
-  ) {}
+  constructor(private readonly addHashtagRepository: AddHashtagRepository) {}
 
   async add(data: AddHashtag.Params): Promise<void> {
-    const id: string = await this.makeId.makeId();
-    await this.addHashtagRepository.add({ ...data, id });
+    await this.addHashtagRepository.add({ ...data });
   }
 }
