14440808b6ad20976a617ac83916dac2add691bd
senugbin9850 <tanja1206@naver.com>
2021-04-22 11:55:09 +0900

    [ADD] search tag posts controller

---
+++ src/presentation/controllers/post/search-tag-posts-controller.ts
@@ -0,0 +1,50 @@
+import { SearchTagPosts } from "@/domain/usecases";
+import { ok, serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+import _ from "lodash";
+
+export class SearchTagPostsController implements Controller {
+  constructor(private readonly searchTagPosts: SearchTagPosts) {}
+
+  async handle(
+    request: SearchTagPostsController.Request
+  ): Promise<HttpResponse> {
+    try {
+      const data = await this.searchTagPosts.searchPosts({ tag: request.tag });
+      const response = _.map(data, (e) => {
+        e.isScrap = false;
+        if (
+          e.scraps.findIndex(
+            (i) => i.userId === request.identity && i.postId === e.id
+          ) > -1
+        )
+          e.isScrap = true;
+        e.scraps = e.scraps.length;
+        e.hashtags = e.hashtags.map((e) => e.tag);
+        e.images = e.images.map((e) => e.id);
+        return _.pick(e, [
+          "id",
+          "title",
+          "category",
+          "views",
+          "user.id",
+          "user.nickname",
+          "hashtags",
+          "images",
+          "scraps",
+          "isScrap",
+        ]);
+      });
+      return ok({ data: response });
+    } catch (e) {
+      return serverError(e);
+    }
+  }
+}
+
+export namespace SearchTagPostsController {
+  export type Request = {
+    identity: string;
+    tag: string;
+  };
+}
