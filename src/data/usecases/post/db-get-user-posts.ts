cb12d42a7adfb26a4dbc3a249cb88a4bfe26836e
senugbin9850 <tanja1206@naver.com>
2021-04-26 16:20:13 +0900

    [ADD] get user posts usecase

---
+++ src/data/usecases/post/db-get-user-posts.ts
@@ -0,0 +1,12 @@
+import { GetUserPostsRepository } from "@/data/protocols";
+import { GetUserPosts } from "@/domain/usecases";
+
+export class DbGetUserPosts implements GetUserPosts {
+  constructor(
+    private readonly getUserPostsRepository: GetUserPostsRepository
+  ) {}
+
+  async getUserPosts(data: GetUserPosts.Params): Promise<any> {
+    return await this.getUserPostsRepository.getUserPosts(data);
+  }
+}
