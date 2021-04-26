a7ced77f61dee7e705b363887d06476415d91ce2
senugbin9850 <tanja1206@naver.com>
2021-04-26 16:22:05 +0900

    [ADD] get user posts controller

---
+++ src/presentation/controllers/post/get-user-posts-controller.ts
@@ -0,0 +1,52 @@
+import { GetUserPosts } from "@/domain/usecases";
+import { ok, serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+import _ from "lodash";
+
+export class GetUserPostsController implements Controller {
+  constructor(private readonly getUserPosts: GetUserPosts) {}
+
+  async handle(request: GetUserPostsController.Request): Promise<HttpResponse> {
+    try {
+      const data = await this.getUserPosts.getUserPosts({
+        userId: request.id,
+        page: Number(request.page),
+      });
+      const response = _.map(data, (e) => {
+        e.isScrap = false;
+        if (
+          e.scraps.findIndex(
+            (i) => i.userId === request.identity && i.postId === e.id
+          ) > -1
+        )
+          e.isScrap = true;
+        e.scraps = e.scraps.length;
+        e.hashtags = e.hashtags.map((e) => e.tag);
+        e.images = e.images.map((e) => e.id);
+        return _.pick(e, [
+          "id",
+          "title",
+          "category",
+          "views",
+          "user.id",
+          "user.nickname",
+          "hashtags",
+          "images",
+          "scraps",
+          "isScrap",
+        ]);
+      });
+      return ok({ data: response });
+    } catch (e) {
+      return serverError(e);
+    }
+  }
+}
+
+export namespace GetUserPostsController {
+  export type Request = {
+    identity: string;
+    id: string;
+    page: string;
+  };
+}
