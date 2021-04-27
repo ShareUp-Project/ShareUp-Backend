e09a8801d30cf0f39505ee7b1bda95187d544150
senugbin9850 <tanja1206@naver.com>
2021-04-27 15:37:35 +0900

    [ADD] change nickname usecase

---
+++ src/data/usecases/user/db-change-nickname.ts
@@ -0,0 +1,12 @@
+import { ChangeNicknameRepository } from "@/data/protocols";
+import { ChangeNickname } from "@/domain/usecases";
+
+export class DbChangeNickname implements ChangeNickname {
+  constructor(
+    private readonly changeNicknameRepository: ChangeNicknameRepository
+  ) {}
+
+  async changeNickname(data: ChangeNickname.Params): Promise<void> {
+    await this.changeNicknameRepository.changeNickname(data);
+  }
+}
