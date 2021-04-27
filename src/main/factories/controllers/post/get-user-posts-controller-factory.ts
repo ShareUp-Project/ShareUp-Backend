83dc8875b0300a968b9610c741ca67828a6d12ed
senugbin9850 <tanja1206@naver.com>
2021-04-27 10:52:03 +0900

    [FIX] import path @ alias

--- src/main/factories/controllers/post/get-user-posts-controller-factory.ts
+++ src/main/factories/controllers/post/get-user-posts-controller-factory.ts
@@ -1,6 +1,6 @@
 import { GetUserPostsController } from "@/presentation/controllers";
 import { Controller } from "@/presentation/protocols";
-import { makeDbGetUserPosts } from "../../usecases";
+import { makeDbGetUserPosts } from "@/main/factories";
 
 export const makeGetUserPostsController = (): Controller => {
   return new GetUserPostsController(makeDbGetUserPosts());
