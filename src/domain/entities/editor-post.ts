87c0e7d2b21ea6ff88f25dcb265d882e2c7870aa
senugbin9850 <tanja1206@naver.com>
2021-04-28 10:08:32 +0900

    [ADD] editor post entity

---
+++ src/domain/entities/editor-post.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity({ name: "editor_post" })
+export class EditorPost {
+  @PrimaryColumn({ length: 45 })
+  id: string;
+
+  @Column({ length: 50 })
+  title: string;
+
+  @Column({ length: 1000 })
+  content: string;
+
+  @Column({ length: 100 })
+  image: string;
+}
