4575311eb53bcbe36e64dcd33b602e6b316c398d
senugbin9850 <tanja1206@naver.com>
2021-04-28 10:07:52 +0900

    [ADD] editor auth usecase

---
+++ src/data/usecases/auth/db-editor-auth.ts
@@ -0,0 +1,13 @@
+import { Encrypter } from "@/data/protocols";
+import { EditorAuth } from "@/domain/usecases";
+import env from "@/main/config/env";
+
+export class DbEditorAuth implements EditorAuth {
+  constructor(private readonly encrypter: Encrypter) {}
+
+  async editorAuth(data: EditorAuth.Params): Promise<EditorAuth.Result> {
+    if (env.editor.id === data.id && env.editor.password === data.password)
+      return { accessToken: await this.encrypter.encryptEditor(data.id) };
+    return null;
+  }
+}
