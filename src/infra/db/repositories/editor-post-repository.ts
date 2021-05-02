8f7d37fb22e132bdc67d71dc7deed3f504248343
senugbin9850 <tanja1206@naver.com>
2021-05-03 08:38:42 +0900

    [ADD] get editor posts repository

--- src/infra/db/repositories/editor-post-repository.ts
+++ src/infra/db/repositories/editor-post-repository.ts
@@ -1,9 +1,13 @@
-import { WriteEditorPostRepository } from "@/data/protocols";
+import {
+  GetEditorPostsRepository,
+  WriteEditorPostRepository,
+} from "@/data/protocols";
 import { EditorPost } from "@/domain/entities";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(EditorPost)
-export class EditorPostRepository implements WriteEditorPostRepository {
+export class EditorPostRepository
+  implements WriteEditorPostRepository, GetEditorPostsRepository {
   async write(data: WriteEditorPostRepository.Params): Promise<void> {
     await getRepository(EditorPost)
       .createQueryBuilder("editor_post")
@@ -11,5 +15,13 @@ export class EditorPostRepository implements WriteEditorPostRepository {
       .into(EditorPost)
       .values(data)
       .execute();
+  }
+
+  async getEditorPosts(data: GetEditorPostsRepository.Params): Promise<any> {
+    return await getRepository(EditorPost)
+      .createQueryBuilder("editor_post")
+      .offset(data.page * 10)
+      .limit(10)
+      .getMany();
   }
 }
