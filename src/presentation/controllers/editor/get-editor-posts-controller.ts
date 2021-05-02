9baeba9fa15cdac5f84cf5c90082876c4e9df249
senugbin9850 <tanja1206@naver.com>
2021-05-03 08:40:00 +0900

    [ADD] get editor posts controller

---
+++ src/presentation/controllers/editor/get-editor-posts-controller.ts
@@ -0,0 +1,26 @@
+import { GetEditorPosts } from "@/domain/usecases";
+import { ok, serverError } from "@/presentation/helpers";
+import { Controller, HttpResponse } from "@/presentation/protocols";
+
+export class GetEditorPostsController implements Controller {
+  constructor(private readonly getEditorPosts: GetEditorPosts) {}
+
+  async handle(
+    request: GetEditorPostsController.Request
+  ): Promise<HttpResponse> {
+    try {
+      const data = await this.getEditorPosts.getEditorPosts({
+        page: Number(request.page),
+      });
+      return ok(data);
+    } catch (e) {
+      return serverError(e);
+    }
+  }
+}
+
+export namespace GetEditorPostsController {
+  export type Request = {
+    page: string;
+  };
+}
