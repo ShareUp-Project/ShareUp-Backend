ad5ef587bc3d641a248508ffebced17e89a70a71
senugbin9850 <tanja1206@naver.com>
2021-05-03 08:37:56 +0900

    [ADD] get editor posts usecase

---
+++ src/data/usecases/editor/db-get-editor-posts.ts
@@ -0,0 +1,12 @@
+import { GetEditorPostsRepository } from "@/data/protocols";
+import { GetEditorPosts } from "@/domain/usecases";
+
+export class DbGetEditorPosts implements GetEditorPosts {
+  constructor(
+    private readonly getEditorPostsRepository: GetEditorPostsRepository
+  ) {}
+
+  async getEditorPosts(data: GetEditorPosts.Params): Promise<any> {
+    return this.getEditorPostsRepository.getEditorPosts(data);
+  }
+}
