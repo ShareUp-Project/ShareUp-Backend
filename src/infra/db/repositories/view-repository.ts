fed5b453b0803f458b2e16165bff1871040150f0
senugbin9850 <tanja1206@naver.com>
2021-05-03 16:10:38 +0900

    [ADD] view repository

---
+++ src/infra/db/repositories/view-repository.ts
@@ -0,0 +1,26 @@
+import { AddViewRepository, FindOneViewRepository } from "@/data/protocols";
+import { View } from "@/domain/entities";
+import { EntityRepository, getRepository } from "typeorm";
+
+@EntityRepository(View)
+export class ViewRepository
+  implements AddViewRepository, FindOneViewRepository {
+  public async add(data: AddViewRepository.Params): Promise<void> {
+    await getRepository(View)
+      .createQueryBuilder("view")
+      .insert()
+      .into(View)
+      .values(data)
+      .execute();
+  }
+
+  public async findOne(
+    data: FindOneViewRepository.Params
+  ): Promise<FindOneViewRepository.Result> {
+    return await getRepository(View)
+      .createQueryBuilder("view")
+      .where("userId = :userId", { userId: data.userId })
+      .andWhere("postId =: postId", { postId: data.postId })
+      .getOne();
+  }
+}
