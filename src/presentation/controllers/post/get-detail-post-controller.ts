4a2540a147dd8a95b67479a06734ce5d43110b45
senugbin9850 <tanja1206@naver.com>
2021-05-03 16:11:42 +0900

    [ADD] add view

--- src/presentation/controllers/post/get-detail-post-controller.ts
+++ src/presentation/controllers/post/get-detail-post-controller.ts
@@ -1,16 +1,28 @@
-import { GetDetailPost } from "@/domain/usecases";
+import { AddView, FindOneView, GetDetailPost } from "@/domain/usecases";
 import { notFound, ok } from "@/presentation/helpers";
 import { Controller, HttpResponse } from "@/presentation/protocols";
 import _ from "lodash";
 
 export class GetDetailPostController implements Controller {
-  constructor(private readonly getDetailPost: GetDetailPost) {}
-
+  constructor(
+    private readonly getDetailPost: GetDetailPost,
+    private readonly addView: AddView,
+    private readonly findOneView: FindOneView
+  ) {}
   async handle(
     request: GetDetailPostController.Request
   ): Promise<HttpResponse> {
     try {
       const data = await this.getDetailPost.getDetail({ ...request });
+      const view = await this.findOneView.findOne({
+        userId: request.identity,
+        postId: request.id,
+      });
+      if (!view)
+        await this.addView.add({
+          userId: request.identity,
+          postId: request.id,
+        });
       data.isScrap = false;
       data.isMine = false;
       if (
@@ -21,6 +33,7 @@ export class GetDetailPostController implements Controller {
         data.isScrap = true;
       if (data.user.id === request.identity) data.isMine = true;
       data.scraps = data.scraps.length;
+      data.views = data.views.length;
       data.images = data.images.map((e) => e.id);
       const response = _.pick(data, [
         "id",
