8f89f833a72482c89a28421bb3bc6388339e1cbf
senugbin9850 <tanja1206@naver.com>
2021-05-05 15:18:51 +0900

    [ADD] find one weekly view repository protocol

---
+++ src/data/protocols/db/weekly-view/find-one-weekly-view-repository.ts
@@ -0,0 +1,12 @@
+import { FindOneWeeklyView } from "@/domain/usecases";
+
+export interface FindOneWeeklyViewRepository {
+  findOne: (
+    data: FindOneWeeklyViewRepository.Params
+  ) => Promise<FindOneWeeklyViewRepository.Result>;
+}
+
+export namespace FindOneWeeklyViewRepository {
+  export type Params = FindOneWeeklyView.Params;
+  export type Result = FindOneWeeklyView.Result;
+}
