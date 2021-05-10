728c8d7ef384754677378a2a14b414dcea27e408
senugbin9850 <tanja1206@naver.com>
2021-05-10 10:12:33 +0900

    [ADD] scheduler

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,11 +3,13 @@ import "reflect-metadata";
 import env from "@/main/config/env";
 import app from "@/main/config/app";
 import { connection } from "@/infra/db";
+import { interval } from "./schedule";
 
 const PORT: number = env.port;
 
 async function startServer() {
   await connection();
+  interval;
   app.listen(PORT, () => {
     console.log(`server on port ${PORT}`);
   });
