ae85b63bda9c2ab0077ea2d7d5a685bfc4d4762f
senugbin9850 <tanja1206@naver.com>
2021-05-10 10:12:27 +0900

    [ADD] scheduler

---
+++ src/main/schedule.ts
@@ -0,0 +1,11 @@
+import { WeeklyViewRepository } from "@/infra/db";
+import schedule from "node-schedule";
+
+export const interval = schedule.scheduleJob("0 0 0 ? * MON *", async () => {
+  try {
+    const weeklyViewRepository = new WeeklyViewRepository();
+    await weeklyViewRepository.deleteAll();
+  } catch (e) {
+    console.log(e.message);
+  }
+});
