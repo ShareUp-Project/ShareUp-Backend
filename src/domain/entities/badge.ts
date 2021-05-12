a161f635fa20ceb90ff1152008accb07d8bbfe69
senugbin9850 <tanja1206@naver.com>
2021-05-12 14:45:44 +0900

    [ADD] badge entity

---
+++ src/domain/entities/badge.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Badge {
+  @PrimaryColumn({ length: 45, name: "user_id" })
+  userId: string;
+
+  @Column({ default: 0 })
+  paper: number;
+
+  @Column({ default: 0 })
+  plastic: number;
+
+  @Column({ default: 0 })
+  glass: number;
+
+  @Column({ default: 0 })
+  styrofoam: number;
+
+  @Column({ default: 0 })
+  vinyl: number;
+
+  @Column({ default: 0 })
+  can: number;
+
+  @Column({ default: 0 })
+  clothing: number;
+}
