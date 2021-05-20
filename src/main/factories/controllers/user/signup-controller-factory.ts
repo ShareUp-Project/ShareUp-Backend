7b01aabadda60e2999d8eee2bab226e6e067a6c6
senugbin9850 <tanja1206@naver.com>
2021-05-20 15:42:57 +0900

    [ADD] create badge

--- src/main/factories/controllers/user/signup-controller-factory.ts
+++ src/main/factories/controllers/user/signup-controller-factory.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from "@/presentation/controllers";
-import { makeDbCreateUser, makeDbFindUser } from "@/main/factories";
+import { makeDbCreateUser, makeDbFindUser, makeDbCreateBadge } from "@/main/factories";
 import { Controller } from "@/presentation/protocols";
 
 export const makeSignupController = (): Controller => {
-  return new SignUpController(makeDbCreateUser(), makeDbFindUser());
+  return new SignUpController(makeDbCreateUser(), makeDbFindUser(), makeDbCreateBadge());
 };
--- src/presentation/controllers/user/signup-controller.ts
+++ src/presentation/controllers/user/signup-controller.ts
@@ -1,15 +1,20 @@
-import { CreateUser, FindUser } from "@/domain/usecases";
+import { CreateBadge, CreateUser, FindUser } from "@/domain/usecases";
 import { Controller, HttpResponse } from "@/presentation/protocols";
 import { alreadyExistsData, ok, serverError } from "@/presentation/helpers";
 
 export class SignUpController implements Controller {
-  constructor(private readonly createUser: CreateUser, private readonly findUser: FindUser) {}
+  constructor(
+    private readonly createUser: CreateUser,
+    private readonly findUser: FindUser,
+    private readonly createBadge: CreateBadge
+  ) {}
 
   async handle(request: SignUpController.Request): Promise<HttpResponse> {
     try {
       const user = await this.findUser.findOne(request.phone);
       if (user) return alreadyExistsData();
       await this.createUser.create(request);
+      await this.createBadge.create({ userId: request.id });
       return ok({ message: "success" });
     } catch (e) {
       return serverError(e);
