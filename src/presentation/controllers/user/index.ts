d5aef95bd9d911d6b31a4be9b971ce9dad837369
seungbin9850 <tanja1206@naver.com>
2021-05-23 02:25:03 +0900

    [ADD] index

--- src/presentation/controllers/user/index.ts
+++ src/presentation/controllers/user/index.ts
@@ -3,3 +3,4 @@ export * from "./nickname-check-controller";
 export * from "./change-password-controller";
 export * from "./get-nickname-controller";
 export * from "./change-nickname-controller";
+export * from "./get-badges-controller";
