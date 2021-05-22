c7dcf6bda4cd5615553f70153a4f64d34b37fe5c
seungbin9850 <tanja1206@naver.com>
2021-05-23 01:52:01 +0900

    [ADD] upgrade badge repository

--- src/infra/db/repositories/badge-repository.ts
+++ src/infra/db/repositories/badge-repository.ts
@@ -1,10 +1,24 @@
-import { CreateBadgeRepository } from "@/data/protocols";
+import { CreateBadgeRepository, GetAllBadgesRepository, UpgradeBadgeRepository } from "@/data/protocols";
 import { Badge } from "@/domain/entities";
 import { EntityRepository, getRepository } from "typeorm";
 
 @EntityRepository(Badge)
-export class BadgeRepository implements CreateBadgeRepository {
+export class BadgeRepository implements CreateBadgeRepository, GetAllBadgesRepository, UpgradeBadgeRepository {
   public async create(data: CreateBadgeRepository.Params): Promise<void> {
     await getRepository(Badge).createQueryBuilder("badge").insert().into(Badge).values(data).execute();
+  }
+
+  public async getAllBadges(data: GetAllBadgesRepository.Params): Promise<GetAllBadgesRepository.Result> {
+    return await getRepository(Badge)
+      .createQueryBuilder("badge")
+      .where("badge.user_id = :userId", { userId: data.userId })
+      .getOne();
+  }
+
+  public async upgrade(data: UpgradeBadgeRepository.Params): Promise<void> {
+    const repo = await getRepository(Badge);
+    const badge = await repo.findOne(data.userId);
+    badge[data.category]++;
+    await repo.save(badge);
   }
 }
