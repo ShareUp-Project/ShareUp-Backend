30c01be00778dcbd1be33a3ecd51f4a537c9d3aa
seungbin9850 <tanja1206@naver.com>
2021-05-23 02:31:54 +0900

    [ADD]  select attributes

--- src/presentation/controllers/user/get-badges-controller.ts
+++ src/presentation/controllers/user/get-badges-controller.ts
@@ -1,6 +1,7 @@
 import { GetAllBadges } from "@/domain/usecases";
 import { ok, serverError } from "@/presentation/helpers";
 import { Controller, HttpResponse } from "@/presentation/protocols";
+import _ from "lodash";
 
 export class GetBadgesController implements Controller {
   constructor(private readonly getAllBadges: GetAllBadges) {}
@@ -8,7 +9,8 @@ export class GetBadgesController implements Controller {
   async handle(request: GetBadgesController.Request): Promise<HttpResponse> {
     try {
       const badges = await this.getAllBadges.getAllBadges({ userId: request.identity });
-      return ok(badges);
+      const response = _.pick(badges, ["first", "paper", "plastic", "glass", "styrofoam", "vinyl", "can", "clothing"]);
+      return ok(response);
     } catch (e) {
       return serverError(e);
     }
