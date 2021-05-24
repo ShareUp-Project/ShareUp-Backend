3418c8f9e358886fc9b7f47faba005e527d2a5d5
seungbin9850 <tanja1206@naver.com>
2021-05-24 10:26:12 +0900

    [ADD] set badge repository

--- src/infra/db/repositories/user-repository.ts
+++ src/infra/db/repositories/user-repository.ts
@@ -4,6 +4,7 @@ import {
   CreateUserRepository,
   FindUserRepository,
   GetNicknameRepository,
+  SetBadgeRepository,
 } from "@/data/protocols";
 import { User } from "@/domain/entities";
 import { EntityRepository, getRepository } from "typeorm";
@@ -15,23 +16,19 @@ export class UserRepository
     FindUserRepository,
     ChangePasswordRepository,
     GetNicknameRepository,
-    ChangeNicknameRepository {
+    ChangeNicknameRepository,
+    SetBadgeRepository
+{
   public async create(data: CreateUserRepository.Params): Promise<void> {
     await getRepository(User).createQueryBuilder("user").insert().into(User).values(data).execute();
   }
 
   public async findOne(phone: string): Promise<FindUserRepository.Result> {
-    return await getRepository(User)
-      .createQueryBuilder("user")
-      .where("phone = :phone", { phone })
-      .getOne();
+    return await getRepository(User).createQueryBuilder("user").where("phone = :phone", { phone }).getOne();
   }
 
   public async findOneByNickname(nickname: string): Promise<FindUserRepository.Result> {
-    return await getRepository(User)
-      .createQueryBuilder("user")
-      .where("nickname = :nickname", { nickname })
-      .getOne();
+    return await getRepository(User).createQueryBuilder("user").where("nickname = :nickname", { nickname }).getOne();
   }
 
   public async change(data: ChangePasswordRepository.Params): Promise<void> {
@@ -43,9 +40,7 @@ export class UserRepository
       .execute();
   }
 
-  public async getNickname(
-    data: GetNicknameRepository.Params
-  ): Promise<GetNicknameRepository.Result> {
+  public async getNickname(data: GetNicknameRepository.Params): Promise<GetNicknameRepository.Result> {
     return await getRepository(User)
       .createQueryBuilder("user")
       .select("user.nickname")
@@ -58,6 +53,15 @@ export class UserRepository
       .createQueryBuilder("user")
       .update(User)
       .set({ nickname: data.nickname })
+      .where("id = :userId", { userId: data.userId })
+      .execute();
+  }
+
+  public async setBadge(data: SetBadgeRepository.Params): Promise<void> {
+    await getRepository(User)
+      .createQueryBuilder("user")
+      .update(User)
+      .set({ badgeCategory: data.category, badgeLevel: data.level })
       .where("id = :userId", { userId: data.userId })
       .execute();
   }
